a7a1a609bcd1ca66eb87f7f99eeaf2dac4355e24
Beatriz <100882891+beatrizslima@users.noreply.github.com>
2024-01-14 22:34:07 -0300

    addTask transition anim

--- frontend/AddTask.js
+++ frontend/AddTask.js
@@ -3,7 +3,7 @@ import { i } from "./lang/langSetup";
 import { useStyles } from "./Styles";
 import { LangContext } from "./lang/langSetup";
 import Backvenn from "./BackvennSvg";
-import { View, Text } from "react-native";
+import { View, Text, Animated, Dimensions } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import {
   TextInput,
@@ -13,8 +13,9 @@ import {
 } from "react-native-paper";
 import { useTheme } from "react-native-paper";
 import DateTimePicker from "@react-native-community/datetimepicker";
+import { useFocusEffect, useBlurEffect } from "@react-navigation/native";
 
-export default function AddTask() {
+export default function AddTask(props) {
   React.useContext(LangContext);
   const { colors } = useTheme();
   const [styles] = useStyles();
@@ -24,113 +25,154 @@ export default function AddTask() {
   const [endDate, setEndDate] = React.useState();
   const [dateType, setDateType] = React.useState("");
 
+  const drawerAnim = React.useRef(
+    new Animated.Value(Dimensions.get("screen").height + 300)
+  ).current;
+
+  useFocusEffect(() => {
+    Animated.timing(drawerAnim, {
+      toValue: 0,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+    return () => {
+      Animated.timing(drawerAnim, {
+        toValue: Dimensions.get("screen").height + 300,
+        duration: 300,
+        useNativeDriver: true,
+      }).start();
+    };
+  });
+
+  function back() {
+    Animated.timing(drawerAnim, {
+      toValue: Dimensions.get("screen").height + 300,
+      duration: 300,
+      useNativeDriver: true,
+    }).start(() => props.navigation.navigate("Home"));
+  }
+
   return (
-    <View style={styles.container}>
-      <View style={{ position: "absolute", top: -180, right: -135 }}>
-        <Backvenn width={400} />
-      </View>
-      <SafeAreaView>
-        <Text style={{ ...styles.h1, marginTop: 100, marginBottom: 60 }}>
-          {i.t("createNew")}
-        </Text>
-        <TextInput
-          label={i.t("labelTaskName")}
-          value={text}
-          onChangeText={(text) => setTitle(text)}
-          mode="outlined"
-          cursorColor={colors.secondary}
-          outlineColor={colors.outline}
-          textColor={colors.onSurface}
-        />
-        <TextInput
-          style={{ marginTop: 20 }}
-          label={i.t("labelTaskDescription")}
-          value={description}
-          onChangeText={(description) => setDescription(description)}
-          mode="outlined"
-          cursorColor={colors.secondary}
-          outlineColor={colors.outline}
-          multiline={true}
-          textColor={colors.onSurface}
-        />
-        <View style={{ flexDirection: "row", marginTop: 20 }}>
-          <SegmentedButtons
-            value={dateType}
-            onValueChange={setDateType}
-            style={{ flex: 1 }}
-            buttons={[
-              {
-                value: "start",
-                icon: "calendar",
-                label: startDate?.toLocaleDateString() || i.t("startDateLabel"),
-                labelStyle: {
-                  textTransform: "none",
-                  fontSize: 15,
-                  color: startDate ? colors.onSurface : colors.subtitle,
-                },
-                style: {
-                  borderColor: colors.outline,
-                  borderWidth: 1,
-                },
-              },
-              {
-                value: "end",
-                icon: "calendar",
-                label: endDate?.toLocaleDateString() || i.t("endDateLabel"),
-                labelStyle: {
-                  textTransform: "none",
-                  fontSize: 15,
-                  color: endDate ? colors.onSurface : colors.subtitle,
-                },
-                style: {
-                  borderColor: colors.outline,
-                  borderWidth: 1,
-                },
-              },
-            ]}
-          />
-          <IconButton
-            icon="close"
-            onPress={() => {
-              setStartDate(undefined);
-              setEndDate(undefined);
-            }}
-            size={16}
-            style={{
-              borderWidth: 1,
-              borderRadius: 4,
-              height: "100%",
-              marginTop: 0,
-              marginRight: 0,
-              borderColor: colors.outline,
-            }}
-          />
-          {dateType == "start" && (
-            <DateTimePicker
-              value={startDate ?? new Date()}
-              mode="date"
-              display="spinner"
-              onChange={(event, date) => {
-                setDateType("");
-                setStartDate(date);
-              }}
-              maximumDate={endDate}
-            />
-          )}
-          {dateType == "end" && (
-            <DateTimePicker
-              value={endDate ?? new Date()}
-              mode="date"
-              display="spinner"
-              onChange={(event, date) => {
-                setDateType("");
-                setEndDate(date);
-              }}
-              minimumDate={startDate}
-            />
-          )}
+    <Animated.View
+      style={{
+        flex: 1,
+        zIndex: 11,
+        transform: [{ perspective: 1000 }, { translateY: drawerAnim }],
+      }}
+    >
+      <View style={styles.container}>
+        <View style={{ position: "absolute", top: -180, right: -135 }}>
+          <Backvenn width={400} />
         </View>
-      </SafeAreaView>
-    </View>
+        <SafeAreaView>
+          <IconButton
+            icon="arrow-left"
+            style={{ position: "absolute", left: -15, top: 30 }}
+            onPress={back}
+          />
+          <Text style={{ ...styles.h1, marginTop: 100, marginBottom: 60 }}>
+            {i.t("createNew")}
+          </Text>
+          <TextInput
+            label={i.t("labelTaskName")}
+            value={text}
+            onChangeText={(text) => setTitle(text)}
+            mode="outlined"
+            cursorColor={colors.secondary}
+            outlineColor={colors.outline}
+            textColor={colors.onSurface}
+          />
+          <TextInput
+            style={{ marginTop: 20 }}
+            label={i.t("labelTaskDescription")}
+            value={description}
+            onChangeText={(description) => setDescription(description)}
+            mode="outlined"
+            cursorColor={colors.secondary}
+            outlineColor={colors.outline}
+            multiline={true}
+            textColor={colors.onSurface}
+          />
+          <View style={{ flexDirection: "row", marginTop: 20 }}>
+            <SegmentedButtons
+              value={dateType}
+              onValueChange={setDateType}
+              style={{ flex: 1 }}
+              buttons={[
+                {
+                  value: "start",
+                  icon: "calendar",
+                  label:
+                    startDate?.toLocaleDateString() || i.t("startDateLabel"),
+                  labelStyle: {
+                    textTransform: "none",
+                    fontSize: 15,
+                    color: startDate ? colors.onSurface : colors.subtitle,
+                  },
+                  style: {
+                    borderColor: colors.outline,
+                    borderWidth: 1,
+                  },
+                },
+                {
+                  value: "end",
+                  icon: "calendar",
+                  label: endDate?.toLocaleDateString() || i.t("endDateLabel"),
+                  labelStyle: {
+                    textTransform: "none",
+                    fontSize: 15,
+                    color: endDate ? colors.onSurface : colors.subtitle,
+                  },
+                  style: {
+                    borderColor: colors.outline,
+                    borderWidth: 1,
+                  },
+                },
+              ]}
+            />
+            <IconButton
+              icon="close"
+              onPress={() => {
+                setStartDate(undefined);
+                setEndDate(undefined);
+              }}
+              size={16}
+              style={{
+                borderWidth: 1,
+                borderRadius: 4,
+                height: "100%",
+                marginTop: 0,
+                marginRight: 0,
+                borderColor: colors.outline,
+              }}
+            />
+            {dateType == "start" && (
+              <DateTimePicker
+                value={startDate ?? new Date()}
+                mode="date"
+                display="spinner"
+                onChange={(event, date) => {
+                  setDateType("");
+                  setStartDate(date);
+                }}
+                maximumDate={endDate}
+              />
+            )}
+            {dateType == "end" && (
+              <DateTimePicker
+                value={endDate ?? new Date()}
+                mode="date"
+                display="spinner"
+                onChange={(event, date) => {
+                  setDateType("");
+                  setEndDate(date);
+                }}
+                minimumDate={startDate}
+              />
+            )}
+          </View>
+        </SafeAreaView>
+      </View>
+    </Animated.View>
   );
 }
--- frontend/App.js
+++ frontend/App.js
@@ -1,6 +1,13 @@
-import { StatusBar } from "expo-status-bar";
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View, useColorScheme } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  useColorScheme,
+  Animated,
+  Dimensions,
+  StatusBar,
+} from "react-native";
 import { darkTheme, lightTheme } from "./Colors";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "./Home";
@@ -11,6 +18,7 @@ import {
   faHouse,
   faSliders,
   faPlusCircle,
+  faCircleXmark,
 } from "@fortawesome/free-solid-svg-icons";
 import { ThemeContext } from "./ThemeContext";
 import { DeviceLanguage, LangContext, i } from "./lang/langSetup";
@@ -27,10 +35,17 @@ export default function App() {
   const langData = { lang, setLang };
   const scheme = useColorScheme();
 
+  let screenHeight = Dimensions.get("screen").height;
+  let windowHeight = Dimensions.get("window").height;
+  let navbarHeight = screenHeight - windowHeight;
+  const [bottomHeight, setBottomHeight] = useState(navbarHeight);
+
+  const tabAnim = React.useRef(new Animated.Value(1)).current;
+
   useEffect(() => {
-    console.log("useEffect App", lang)
+    console.log("useEffect App", lang);
     async function getSettings() {
-      console.log("getsettings App", lang)
+      console.log("getsettings App", lang);
       AsyncStorage.getItem("theme")
         .then((x) => setTheme(x))
         .catch((err) => setTheme(scheme));
@@ -48,49 +63,97 @@ export default function App() {
     if (!theme || !lang) getSettings();
   }, [lang]);
 
-
+  function stateChange(evt) {
+    if (evt.index == 1) {
+      Animated.timing(tabAnim, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: true,
+      }).start(() => setBottomHeight(0));
+    } else {
+      const screenHeight = Dimensions.get("screen").height;
+      const windowHeight = Dimensions.get("window").height;
+      const navbarHeight = screenHeight - windowHeight;
+      setBottomHeight(navbarHeight);
+      Animated.timing(tabAnim, {
+        toValue: 1,
+        duration: 300,
+        useNativeDriver: true,
+      }).start();
+    }
+  }
 
   return (
     <LangContext.Provider value={langData}>
       <ThemeContext.Provider value={themeData}>
         <PaperProvider theme={theme === "dark" ? darkTheme : lightTheme}>
-          <NavigationContainer theme={theme === "dark" ? darkTheme : lightTheme}>
-            <Tab.Navigator>
-              <Tab.Screen
-                options={{
-                  headerShown: false,
-                  tabBarShowLabel: false,
-                  tabBarIcon: ({ focused, color, size }) => (
-                    <FontAwesomeIcon icon={faHouse} color={color} size={size * focused ? 23 : 18} />
-                  ),
+          <View
+            style={{
+              flex: 1,
+              backgroundColor:
+                theme === "dark"
+                  ? darkTheme.colors.background
+                  : lightTheme.colors.background,
+            }}
+          >
+            <NavigationContainer
+              theme={theme === "dark" ? darkTheme : lightTheme}
+              onStateChange={stateChange}
+            >
+              <Tab.Navigator
+                screenOptions={{
+                  unmountOnBlur: true,
+                  tabBarStyle: { opacity: tabAnim, height: bottomHeight },
                 }}
-                name="Home"
-                component={Home}
-              />
-              <Tab.Screen
-                options={{
-                  headerShown: false,
-                  tabBarShowLabel: false,
-                  tabBarIcon: ({ focused, color, size }) => (
-                    <FontAwesomeIcon icon={faPlusCircle} color={color} size={size * focused ? 23 : 18} />
-                  ),
-                }}
-                name="Add"
-                component={AddTask}
-              />
-              <Tab.Screen
-                options={{
-                  headerShown: false,
-                  tabBarShowLabel: false,
-                  tabBarIcon: ({ focused, color, size }) => (
-                    <FontAwesomeIcon icon={faSliders} color={color} size={size * focused ? 23 : 18} />
-                  ),
-                }}
-                name="Settings"
-                component={Settings}
-              />
-            </Tab.Navigator>
-          </NavigationContainer>
+              >
+                <Tab.Screen
+                  options={{
+                    headerShown: false,
+                    tabBarShowLabel: false,
+                    tabBarIcon: ({ focused, color, size }) => (
+                      <FontAwesomeIcon
+                        icon={faHouse}
+                        color={color}
+                        size={size * focused ? 23 : 18}
+                      />
+                    ),
+                  }}
+                  name="Home"
+                  component={Home}
+                />
+                <Tab.Screen
+                  options={{
+                    headerShown: false,
+                    tabBarShowLabel: false,
+                    tabBarIcon: ({ focused, color, size }) => (
+                      <FontAwesomeIcon
+                        icon={faPlusCircle}
+                        color={color}
+                        size={size * focused ? 23 : 18}
+                      />
+                    ),
+                  }}
+                  name="Add"
+                  component={AddTask}
+                />
+                <Tab.Screen
+                  options={{
+                    headerShown: false,
+                    tabBarShowLabel: false,
+                    tabBarIcon: ({ focused, color, size }) => (
+                      <FontAwesomeIcon
+                        icon={faSliders}
+                        color={color}
+                        size={size * focused ? 23 : 18}
+                      />
+                    ),
+                  }}
+                  name="Settings"
+                  component={Settings}
+                />
+              </Tab.Navigator>
+            </NavigationContainer>
+          </View>
         </PaperProvider>
       </ThemeContext.Provider>
     </LangContext.Provider>
